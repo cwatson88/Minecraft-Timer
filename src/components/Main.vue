974ab1af72bd1d7075861ec5f570dfb5b925b026
Chris Watson <cwatson1988@gmail.com>
2017-09-22 23:14:29 +0100

    second commit

---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="name">
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+    data: () => ({
+        dataOne: "data object here"
+    }),
+    computed: {
+    },
+    methods: {
+        ...mapMutations(['..']),
+        ...mapActions(['...', '..']),
+        // Start method functions
+        methodOne() {
+            //method stuff here
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+// @import '../../global-styles.scss';
+</style>
